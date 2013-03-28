b9400ef4b8fcdff7982942f167e0a60f3f6c2b09
Boy de Wit <boydewit@b3partners.nl>
2013-03-28 17:10:21 +0100

    added Double member;

--- src/Renamed.java
+++ src/Renamed.java
@@ -1,6 +1,7 @@
 public class Renamed {
     private String str = "Hello World!";
     private Integer number;
+    private Double enkelEnkelDubbelDubbel;
     
     public void sayHello() {
         System.out.println(str);
